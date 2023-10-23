c74b9a67becee83a153aa4e33369df99e4741552
NoseAlien <k021g1117@g.neec.ac.jp>
2023-10-23 16:44:29 +0900

    02_05途中まで

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -1,5 +1,6 @@
 #include "TextureConverter.h"
-#include <DirectXTex.h>
+
+using namespace DirectX;
 
 void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath)
 {
@@ -8,7 +9,63 @@ void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath)
 
 void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
 {
+	HRESULT result;
+
 	std::wstring wFilePath = ConvertMultibyteStringToWideString(filePath);
+
+	result = LoadFromWICFile(wFilePath.c_str(), WIC_FLAGS_NONE, &metadata_, scratchImage_);
+	assert(SUCCEEDED(result));
+
+	//フォルダパスとファイル名を分離する
+	SeparateFilePath(wFilePath);
+}
+
+void TextureConverter::SeparateFilePath(const std::wstring& filePath)
+{
+	size_t pos1;
+	std::wstring exceptExt;
+
+	//区切り文字'.'が出てくる一番最後の部分を検索
+	pos1 = filePath.rfind('.');
+	//検索がヒットしたら
+	if (pos1 != std::wstring::npos)
+	{
+		//区切り文字の後ろをファイル拡張子として保存
+		fileExt_ = filePath.substr(pos1 + 1, filePath.size() - pos1 - 1);
+		//区切り文字の前までを抜き出す
+		exceptExt = filePath.substr(0, pos1);
+	}
+	else
+	{
+		fileExt_ = L"";
+		exceptExt = filePath;
+	}
+
+	//区切り文字'\\'が出てくる一番最後の部分を検索
+	pos1 = filePath.rfind('\\');
+	//検索がヒットしたら
+	if (pos1 != std::wstring::npos)
+	{
+		//区切り文字の前までをディレクトリパスとして保存
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+		//区切り文字の後ろをファイル名として保存
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+		return;
+	}
+
+	//区切り文字'/'が出てくる一番最後の部分を検索
+	pos1 = filePath.rfind('/');
+	//検索がヒットしたら
+	if (pos1 != std::wstring::npos)
+	{
+		//区切り文字の前までをディレクトリパスとして保存
+		directoryPath_ = exceptExt.substr(0, pos1 + 1);
+		//区切り文字の後ろをファイル名として保存
+		fileName_ = exceptExt.substr(pos1 + 1, exceptExt.size() - pos1 - 1);
+		return;
+	}
+	directoryPath_ = L"";
+	fileName_ = exceptExt;
 }
 
 std::wstring TextureConverter::ConvertMultibyteStringToWideString(const std::string& mString)
--- TextureConverter.h
+++ TextureConverter.h
@@ -1,10 +1,27 @@
 #pragma once
 #include <string>
+#include <DirectXTex.h>
+
 class TextureConverter
 {
+private:
+	//画像の情報
+	DirectX::TexMetadata metadata_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
+
 public:
 	void ConvertTextureWICToDDS(const std::string& filePath);
 	void LoadWICTextureFromFile(const std::string& filePath);
+
+private:
+	void SeparateFilePath(const std::wstring& filePath);
 
 private:
 	static std::wstring ConvertMultibyteStringToWideString(const std::string& mString);
