c55eccf5833a70aad6aa7777db3a2d895b473dc3
NoseAlien <k021g1117@g.neec.ac.jp>
2023-11-13 14:29:22 +0900

    02_08完了

--- Resources/convert.ps1
+++ Resources/convert.ps1
@@ -2,6 +2,6 @@ $files = Get-Item *.png
 
 foreach($f in $files)
 {
-	Start-Process -FilePath TextureConverter.exe -ArgumentList $f -Wait
+	Start-Process -FilePath TextureConverter.exe -ArgumentList $f ," -ml 1" -Wait
 }
 pause
